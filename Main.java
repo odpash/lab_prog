60212fac67d2e27fff4168755391ace907963d24
odpash <87781951+odpash@users.noreply.github.com>
2021-09-29 12:06:20 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,46 +1,50 @@
+package com.company;
+
+import java.lang.Math.*;
+
 public class Main {
     public static void main(String[] args) {
         // Step # 1
-        int a[] = new int[(22 - 2) / 2 + 1];
+        int a[] = new int[(23 - 5) / 2 + 1];
         int idx = -1;
-        for (int i = 22; i > 1; i -= 2) {
+        for (int i = 23; i > 4; i -= 2) {
             idx += 1;
             a[idx] = i;
         }
         // Step # 2
-        double x[] = new double[10];
-        for (int i = 0; i < 10; i++) {
-            x[i] = -3 + Math.random() * 10;
+        double x[] = new double[11];
+        for (int i = 0; i < 11; i++) {
+            x[i] = -14 + Math.random() * 25;
         }
+
         // Step # 3
-        double sp[][] = new double[11][10];
+        double sp[][] = new double[10][11];
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (a[i] == 20) {
-                    sp[i][j] = Math.pow(2 * Math.asin(Math.pow((x[j] + 2) * Math.E + 1, 2)), 3);
-                } else if (a[i] == 2 || a[i] == 6 || a[i] == 10 || a[i] == 12 || a
-                        [i] == 14) {
-                    sp[i][j] = Math.asin(0.5 * Math.pow(Math.E, -Math.abs(x[j])));
+            for (int j = 0; j < 11; j++) {
+                if (a[i] == 9) {
+                    sp[i][j] = Math.cbrt(Math.pow(2 * (Math.pow((3 / x[j]), (x[j])) + 1), Math.pow((2 / 3 * (1 - x[j])), 3)));
+                } else if (a[i] == 5 || a[i] == 11 || a[i] == 15 || a[i] == 17 || a[i] == 19) {
+                    sp[i][j] = Math.cos(Math.cos(Math.pow(x[j] / (x[j] + 1), 2)));
                 } else {
-                    sp[i][j] = Math.pow((0.5 - Math.pow(Math.pow(2 * Math.atan(x[j] + 2 * Math.E + 1), Math.cos(x[j])), 2))  / 3, 3);
+                    sp[i][j] = Math.pow(Math.log(Math.sqrt(Math.pow(Math.tan(x[j]), 2))), 3);
                 }
             }
         }
         System.out.println("Output:");
-        System.out.println("Col/Row | Col 1 | Col 2 | Col 3 | Col 4 | Col 5 | Col 6 | Col 7 | Col 8 | Col 9 | Col 10");
-        //System.out.println("-".repeat(88));
-        for (int i = 0; i < 11; i++){
-            String to_print;
+        System.out.println("Col/Row |   Col 1   |   Col 2   |   Col 3   |   Col 4   |   Col 5   |   Col 6   |   Col 7   |   Col 8   |   Col 9   |   Col 10  |   Col 11  |");
+        for (int i = 0; i < 10; i++) {
+            String to_print = "";
             if (i > 8) {to_print = " Row " + (i + 1) + " |";}
             else {to_print = " Row " + (i + 1) + "  |";}
-            for (int j = 0; j < 10; j++) {
-                if (Double.isNaN(sp[i][j])) {to_print += "  " + String.format("%.3f", sp[i][j]) + "  |";}
-                else if (String.format("%.3f", sp[i][j]).length() == 5) {to_print += " " + String.format("%.3f", sp[i][j]) + " |";}
-                else if (String.format("%.3f", sp[i][j]).length() == 6) {to_print += "" + String.format("%.3f", sp[i][j]) + " |";}
-                else if (String.format("%.3f", sp[i][j]).length() == 7) {to_print += String.format("%.3f", sp[i][j]) + "|";}
-            }
+            for (int j = 0; j < 11; j++) {
+                if (String.format("%.5f", sp[i][j]).length() == 7) {to_print += "  " + String.format("%.5f", sp[i][j]) + "  |";};
+                if (String.format("%.5f", sp[i][j]).length() == 8) {to_print += "  " + String.format("%.5f", sp[i][j]) + " |";};
+                if (String.format("%.5f", sp[i][j]).length() == 9) {to_print += " " + String.format("%.5f", sp[i][j]) + " |";};
+                if (String.format("%.5f", sp[i][j]).length() == 10) {to_print += "" + String.format("%.5f", sp[i][j]) + " |";};
+                if (String.format("%.5f", sp[i][j]).length() == 11) {to_print += "" + String.format("%.5f", sp[i][j]) + "|";};
+
+                }
             System.out.println(to_print);
         }
-        //System.out.println("-".repeat(88));
     }
 }
